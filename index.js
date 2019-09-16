adf1c6e7fff900384d20cf74ae8701f67c06e205
YaroslavPikiner <yaroyay@gmail.com>
2019-09-16 17:31:53 +0300

    add basic structure

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+
+const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine )
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.render('index.hbs');
+})
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs');
+})
+
+const PORT = process.env.PORT || 3004;
+
+app.listen(PORT, () => {
+    console.log("Server is running")
+})
